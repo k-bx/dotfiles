ec3a8a34db17b271b260ea972db74f05c21f1a02
Kostiantyn Rybnikov <k-bx@k-bx.com>
2018-04-04 17:59:36 +0300

    A bit more contrast pre color in solarized-light

--- dotemacs.el
+++ dotemacs.el
@@ -434,8 +434,7 @@
  '(jabber-title-medium ((t (:inherit variable-pitch :weight bold :height 1.2 :width expanded))))
  '(juick-user-name-face ((t (:foreground "deep sky blue" :slant normal :weight bold))))
  '(magit-log-head-label-wip ((t (:background "Grey95" :foreground "LightSkyBlue3"))))
- '(markdown-pre-face ((t (:inherit default :foreground "#545e5e"))))
- '(default ((t (:foreground "#3d4444")))))
+ '(markdown-pre-face ((t (:inherit default :foreground "#768282")))))
 
 (require 'expand-region)
 (global-set-key (kbd "C-=") 'er/expand-region)
