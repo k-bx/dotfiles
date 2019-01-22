9d9fc3b88b5904cfe81e0cc6f67bc90c7641efc1
Kostiantyn Rybnikov <k-bx@k-bx.com>
2019-01-22 15:45:12 +0200

    Add --file-watch to build command

--- dotemacs.el
+++ dotemacs.el
@@ -468,9 +468,9 @@
  '(grep-find-template
    "find . <X> -type f <F> -exec grep <C> -nH -e <R> {} + | cut -c 1-300")
  '(haskell-compile-cabal-build-alt-command
-   "cd %s && stack build --fast --ghc-options=\"-j +RTS -A32M -RTS\"")
+   "cd %s && stack build --fast --file-watch --ghc-options=\"-j +RTS -A32M -RTS\"")
  '(haskell-compile-cabal-build-command
-   "cd %s && stack build --fast --ghc-options=\"-j +RTS -A32M -RTS\"")
+   "cd %s && stack build --fast --file-watch --ghc-options=\"-j +RTS -A32M -RTS\"")
  '(haskell-indentation-ifte-offset 2)
  '(haskell-indentation-layout-offset 2)
  '(haskell-indentation-left-offset 2)
