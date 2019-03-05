732ba9313e8a83d212b9c269861af83f2e38c789
Kostiantyn Rybnikov <k-bx@k-bx.com>
2019-03-05 13:28:15 +0200

    Use global-map instead of elm-map

--- dotemacs.el
+++ dotemacs.el
@@ -280,7 +280,7 @@
   (turn-on-subword-mode)
   (add-hook 'elm-mode-hook #'elm-oracle-setup-completion)
   (define-key elm-mode-map (kbd "M-,") 'my-find-tag)
-  (define-key elm-mode-map (kbd "\C-c\C-s") 'elm-mode-format-buffer)
+  (define-key global-map (kbd "\C-c\C-s") 'elm-mode-format-buffer)
   (setq elm-interactive-command '("elm" "repl")
         elm-reactor-command '("elm" "reactor")
         elm-compile-command '("elm" "make")
