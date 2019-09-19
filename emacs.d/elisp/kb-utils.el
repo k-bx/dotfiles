41c6c2d521caa85f5bf36b616f01505c66704730
Kostiantyn Rybnikov <k-bx@k-bx.com>
2019-09-19 16:14:27 +0300

    Indent rigidly

--- emacs.d/elisp/kb-utils.el
+++ emacs.d/elisp/kb-utils.el
@@ -83,24 +83,13 @@ middle"
   (interactive)
   (mapc 'kill-buffer (buffer-list)))
 
-;;
-
-(defun shift-region (distance)
-  (let ((mark (mark)))
-    (save-excursion
-      (indent-rigidly (region-beginning) (region-end) distance)
-      (push-mark mark t t)
-      ;; Tell the command loop not to deactivate the mark
-      ;; for transient mark mode
-      (setq deactivate-mark nil))))
-
 (defun shift-right ()
   (interactive)
-  (shift-region 2))
+  (indent-rigidly (region-beginning) (region-end) 2))
 
 (defun shift-left ()
   (interactive)
-  (shift-region -2))
+  (indent-rigidly (region-beginning) (region-end) -2))
 
 (defun go-shift-region (distance)
   (interactive "nEnter distance: ")
