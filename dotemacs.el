56ae1ee58b47da66f3c2896773bca3e6ae33b718
Kostia R <k-bx@k-bx.com>
2024-07-22 22:30:34 +0300

    disable one more theme

--- dotemacs.el
+++ dotemacs.el
@@ -75,7 +75,8 @@
     js2-mode
     json-reformat
     ;; lsp-mode
-    magit 
+    magit
+	;; magit-delta
     markdown-mode 
     multiple-cursors 
     nix-mode 
@@ -330,6 +331,7 @@
 (require 'magit)
 (setq magit-last-seen-setup-instructions "1.4.0")
 (global-set-key (kbd "C-x g") 'magit-status)
+;; (add-hook 'magit-mode-hook (lambda () (magit-delta-mode +1)))
 
 (setq custom-theme-load-path (cons "~/.emacs.d/themes" custom-theme-load-path))
 
@@ -341,7 +343,9 @@
   (disable-theme 'solarized-dark)
   (disable-theme 'solarized-light-high-contrast)
   (disable-theme 'solarized-dark-high-contrast)
-  (disable-theme 'solarized-zenburn))
+  (disable-theme 'solarized-zenburn)
+  (disable-theme 'solarized-selenized-white)
+  )
 (defun dark ()
   (interactive)
   (disable-all-themes)
@@ -472,6 +476,7 @@
   ;; (eglot-ensure)
   )
 (add-hook 'rust-mode-hook 'my-rust-mode-hook)
+(require 'rust-mode)
 
 ;; docs https://agda.readthedocs.io/en/latest/tools/emacs-mode.html
 ;; (setq load-path (cons "~/workspace/agda/src/data/emacs-mode" load-path))
@@ -731,7 +736,7 @@
  '(markdown-enable-wiki-links t)
  '(ormolu-extra-args '("--ghc-opt" "-XTypeApplications"))
  '(package-selected-packages
-   '(pastelmac-theme apropospriate-theme benchmark-init flycheck-eglot eglot editorconfig quelpa-use-package quelpa just-mode timu-macos-theme ac-geiser geiser-guile helm etags-select flatbuffers-mode casharp-mode company-go go-mode csharp-mode use-package dhall-mode bind-key ormolu format-all dante adaptive-wrap proof-general hasklig-mode string-inflection flycheck-elm add-node-modules-path tide groovy-mode idris-mode multi-term projectile-ripgrep package-build shut-up epl git commander f dash s))
+   '(0x0 0blayout php-mode pastelmac-theme apropospriate-theme benchmark-init flycheck-eglot eglot editorconfig quelpa-use-package quelpa just-mode timu-macos-theme ac-geiser geiser-guile helm etags-select flatbuffers-mode casharp-mode company-go go-mode csharp-mode use-package dhall-mode bind-key ormolu format-all dante adaptive-wrap proof-general hasklig-mode string-inflection flycheck-elm add-node-modules-path tide groovy-mode idris-mode multi-term projectile-ripgrep package-build shut-up epl git commander f dash s))
  '(projectile-generic-command
    "find . -type f -not -name \"*.hi\" -not -name \"*.o\" -not -name \"*.p_o\" -not -name \"*.p_hi\" -not -name \"*.pyc\" -not -path \"*/cabal-dev/*\" -not -path \"*/.cabal-sandbox/*\" -not -path \"*/dist/*\" -not -path \"*/build/*\" -not -path \"*/.git/*\" -not -path \"*/javadoc/*\" -print0")
  '(projectile-switch-project-hook
